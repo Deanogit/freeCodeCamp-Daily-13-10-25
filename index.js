33985c3e874ba3f7a5f35b832feb4de8bb63ecec
Dean Howe <hideanhowe@gmail.com>
2025-12-27 11:23:06 +0000

    testing

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 // Given a string representing a time of the day in the 24-hour format of "HHMM", return the time in its equivalent 12-hour format of "H:MM AM" or "H:MM PM".
 
 // The given input will always be a four-digit string in 24-hour time format, from "0000" to "2359".
+
+function to12(time) {
+  console.log(time);
+
+  if (parseInt(`${time[0]}${time[1]}`) >= 12) {
+    return `${time[0]}${time[1]}:${time[2]}${time[3]} PM`;
+  } else {
+    return `${time[0]}${time[1]}:${time[2]}${time[3]} AM`;
+  }
+
+  // return time;
+}
